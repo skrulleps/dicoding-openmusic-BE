cc20360a0fc3732a2954f7b2dcfbbe74eab408ac
Mochamed Fadhlan Tuhairi <fadhlantuhairi@gmail.com>
2025-07-16 19:32:58 +0700

    Update: to Common JS

--- migrations/1752564634309_table-songs.js
+++ migrations/1752564634309_table-songs.js
@@ -1,14 +1,14 @@
 /**
  * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
  */
-export const shorthands = undefined;
+// export const shorthands = undefined;
 
 /**
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-export const up = (pgm) => {
+exports.up = (pgm) => {
     pgm.createTable('songs', {
         id: {
             type: 'VARCHAR(8)',
@@ -58,6 +58,6 @@ export const up = (pgm) => {
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-export const down = (pgm) => {
+exports.down = (pgm) => {
     pgm.dropTable('songs');
 };
