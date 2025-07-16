5de61a661ae4abfb5a7dba1e5de77856c2f49f67
Mochamed Fadhlan Tuhairi <fadhlantuhairi@gmail.com>
2025-07-16 13:03:41 +0700

    Add: index validator

---
+++ src/validator/songs/index.js
@@ -0,0 +1,11 @@
+const InvariantError = require("../../exceptions/InvariantError");
+const { SongPayloadSchema } = require("./schema");
+
+const validateSongPayload = (payload) => {
+    const validationResult = SongPayloadSchema.validate(payload);
+    if (validationResult.error) {
+        throw new InvariantError(validationResult.error.message);
+    }
+};
+
+module.exports = { validateSongPayload };
