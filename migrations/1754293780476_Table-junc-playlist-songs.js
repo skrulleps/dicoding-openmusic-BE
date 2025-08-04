4abb0e3891e72b2acfd97ef277938ae7431e8639
Mochamed Fadhlan Tuhairi <fadhlantuhairi@gmail.com>
2025-08-04 14:51:01 +0700

    Add: Migrations Junc Table Playlist Songs

---
+++ migrations/1754293780476_Table-junc-playlist-songs.js
@@ -0,0 +1,41 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable('playlist_songs', {
+    id: {
+      type: 'VARCHAR(8)',
+      primaryKey: true,
+    },
+    // eslint-disable-next-line camelcase
+    playlist_id: {
+      type: 'VARCHAR(8)',
+      notNull: true,
+      references: 'playlists(id)',
+      onDelete: 'CASCADE',
+    },
+    // eslint-disable-next-line camelcase
+    song_id: {
+      type: 'VARCHAR(8)',
+      notNull: true,
+      references: 'songs(id)',
+      onDelete: 'CASCADE',
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable('playlist_songs');
+};
