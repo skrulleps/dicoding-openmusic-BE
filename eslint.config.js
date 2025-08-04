55b113963cb3a0ae78b7616c477de6f19a0d6935
Mochamed Fadhlan Tuhairi <fadhlantuhairi@gmail.com>
2025-08-04 13:52:21 +0700

    Add: rules for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,15 @@ module.exports = defineConfig([
   {
     files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
-    extends: ['js/recommended'],
+    extends: [js.configs.recommended],
     languageOptions: { globals: globals.node, sourceType: 'module' },
+    rules: {
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'no-unused-vars': ['error', {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: true,
+      }]
+    }
   },
 ]);
