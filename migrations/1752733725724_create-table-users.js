00f05924afe983307dd9dcc5678d8f3cab68d0a2
Mochamed Fadhlan Tuhairi <fadhlantuhairi@gmail.com>
2025-08-04 13:51:38 +0700

    Add: New Table Users

--- migrations/1752733725724_create-table-users.js
+++ migrations/1752733725724_create-table-users.js
@@ -8,11 +8,33 @@ export const shorthands = undefined;
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-export const up = (pgm) => {};
+export const up = (pgm) => {
+  pgm.createTable('users', {
+    id: {
+      type: 'VARCHAR(8)',
+      primaryKey: true,
+    },
+    username: {
+      type: 'TEXT',
+      notNull: true,
+      unique: true,
+    },
+    password: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    fullname: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
 
 /**
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-export const down = (pgm) => {};
+export const down = (pgm) => {
+  pgm.dropTable('users');
+};
