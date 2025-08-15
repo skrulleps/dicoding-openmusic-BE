08996b89610abec0a537de41eba3026f8b7155ad
Mochamed Fadhlan Tuhairi <fadhlantuhairi@gmail.com>
2025-08-15 17:11:50 +0700

    Add : model album (kriteria 2)

--- src/utils/AlbumModel.js
+++ src/utils/AlbumModel.js
@@ -2,12 +2,14 @@ const AlbumModel = ({
   id,
   name,
   year,
+  cover_url, // eslint-disable-line camelcase
   created_at, // eslint-disable-line camelcase
   updated_at, // eslint-disable-line camelcase
 }) => ({
   id,
   name,
   year,
+  coverUrl: cover_url || null, // eslint-disable-line camelcase
   createdAt: created_at, // eslint-disable-line camelcase
   updatedAt: updated_at, // eslint-disable-line camelcase
 });
